34d90117ad1a7740ea1d96dddf580918163b82d6
Manali Sarkar <manali.sarkar93@gmail.com>
2019-11-15 01:44:17 +0530

    Program to calculate the sum of numbers occurring in an alphanumeric string.

---
+++ SumOfNumbersInString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SumOfNumbersInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = 0;
+
+            Console.WriteLine("------ SUM OF NUMBERS IN STRING ------\n");
+            Console.WriteLine("Enter the number of testcases: ");
+            T = Convert.ToInt32(Console.ReadLine());
+
+            while(T-- > 0)
+            {
+                String str = String.Empty;
+
+                Console.WriteLine("Enter an alphanumeric string: ");
+                str = Console.ReadLine();
+
+                string[] numbers = Regex.Split(str, @"\D+");
+
+                int total = 0;
+
+                foreach (string n in numbers)
+                {
+                    if (!string.IsNullOrEmpty(n))
+                    {
+                        int i = int.Parse(n);
+                        total += i;
+                        //Console.WriteLine("Number: {0}", i);
+                    }
+                }
+
+                Console.WriteLine("Output: " + total);
+            }
+        }
+    }
+}
